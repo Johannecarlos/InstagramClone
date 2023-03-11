ab6b2b6f2fdb381b1bc53d49553a1c1c2547d0f1
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-10 23:02:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
     </div>
   );
 }
