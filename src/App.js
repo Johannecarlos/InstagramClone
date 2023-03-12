d31f6a78e88e7cbaa1e6ea4c4dc515f0af335275
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-12 00:14:07 -0300

    Update App.css & App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
+      <div className="app_header">
+        <img
+          className="app_headerImage"
+        src="https://assets.turbologo.com/blog/pt/2019/09/19133708/instagram-logo-illustration-728x437.png"
+        alt=""
+        />
+      </div>
+      
+        <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
+
+      {/* Header */}
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
