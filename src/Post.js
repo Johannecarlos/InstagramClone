6c99dfac6b2d794bf28af9e94ae0401be8d86ded
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-14 06:53:45 -0300

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -6,8 +6,13 @@ function Post() {
       <h3>Username</h3>
       {/* header -> avatar + username */}
 
+      <img className="post_image" src="https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png"
+      alt=""
+      />
+
       {/* image */}
 
+      <h4>Username: caption</h4>
       {/* username + caption */}
     </div>
   )
