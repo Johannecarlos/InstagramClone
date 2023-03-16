c70ded0ad74e8094075f5b437a5e3a47a11b7bdb
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-15 23:27:06 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Post from './Post';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <div className="app_header">
         <img
           className="app_headerImage"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/320px-Instagram_logo.svg.png"
+        alt=""
         />
       </div>
       
-        <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
+      <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
 
       <Post />
-      {/* Posts */}
-      {/* Posts */}
+      <Post />
+      <Post />
+
     </div>
   );
 }
