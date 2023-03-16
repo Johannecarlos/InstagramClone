1e1d7710e37d3d0cea383b0ed250751e38ec1779
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-15 23:20:27 -0300

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
   return (
-    <div>
-      <h3>Username</h3>
-      {/* header -> avatar + username */}
-
-      <img className="post_image" src="https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png"
-      alt=""
+    <div className="post">
+      <div className="post_header">
+      <Avatar
+        className="post_avatar"
+        alt='RafehQazi'
+        src="/static/images/avatar/1.jpg"
       />
+      <h3>Username</h3>
+      </div>
+      
+      <img className="post_image" src="https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png"/>
 
-      {/* image */}
-
-      <h4>Username: caption</h4>
-      {/* username + caption */}
+      <h4 className="post_text"><strong>cleverqazi</strong>WOW day three of live sessions</h4>
     </div>
   )
 }
