f767c9bc1b7b98d71c243f1b3f4f19a1ab344d29
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-17 23:53:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,6 +28,12 @@ function App() {
       
       <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
 
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl="" />
+        ))
+      }
+
       <Post username="cleverqazi" caption="WOW it works" imageUrl="https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png" />
       <Post username="ssssangha" caption="DOPE" imageUrl="" />
 
