0100a77767c6ed806ac9ae0809dd5424c7406777
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-17 23:47:01 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "cleverqazi",
+      caption: "WOW it works",
+      imageUrl: "https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png"
+    },
+    {
+      username: "cleverqazi",
+      caption: "WOW it works",
+      imageUrl: "https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png"
+    }
+  ]);
+
   return (
     <div className="app">
       <div className="app_header">
         <img
           className="app_headerImage"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/320px-Instagram_logo.svg.png"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
         alt=""
         />
       </div>
@@ -16,8 +29,7 @@ function App() {
       <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
 
       <Post username="cleverqazi" caption="WOW it works" imageUrl="https://cdn-media-1.freecodecamp.org/images/1*y6C4nSvy2Woe0m7bWEn4BA.png" />
-      <Post />
-      <Post />
+      <Post username="ssssangha" caption="DOPE" imageUrl="" />
 
     </div>
   );
