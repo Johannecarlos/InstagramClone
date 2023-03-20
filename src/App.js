51e50bb8dcc49db9ea25b0133fed0ea284079ef5
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-19 23:04:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
+import { db } from './firebase';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "cleverqazi",
-      caption: "WOW it works",
-      imageUrl: "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-    },
-    {
-      username: "cleverqazi",
-      caption: "WOW it works",
-      imageUrl: "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
+
+  // UseEffect -> Runs a piece of code based on a specific condition
+
+  useEffect(() => {
+    // this is where the code runs
+    db.collection('posts').onSnapshot(snapshot => {
+      // every time a new post is added, this code fires...
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+  })
+}, []);
 
   return (
     <div className="app">
@@ -29,13 +32,10 @@ function App() {
       <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
 
       {
-        posts.map(post => (
+        posts.map(({id, post}) => (
           <Post username={post.username} caption={post.caption} imageUrl="" />
         ))
       }
-
-      <Post username="cleverqazi" caption="WOW it works" imageUrl="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" />
-      <Post username="ssssangha" caption="DOPE" imageUrl="" />
 
     </div>
   );
