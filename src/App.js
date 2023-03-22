c9f81836467efebbf1249c56362f0075bfa091e6
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2023-03-21 22:27:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
-import { auth, db } from './firebase';
+import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { Button } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
+
 
 function getModalStyle() {
   const top = 50;
@@ -31,12 +32,31 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
-
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setSignIn] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        // user has logged out...
+        setUser(null);
+      }
+    })
+
+    return () => {
+      // perform some cleanup actions
+      unsubscribe();
+    }
+  }, [user, username]);
 
   useEffect(() => {
     // this is where the code runs
@@ -49,12 +69,23 @@ function App() {
   })
 }, []);
 
-const SignUp = (event) => {
+const signUp = (event) => {
   event.preventDefault();
 
   auth
   .createUserWithEmailAndPassword(email, password)
+  .then((authUser) => {
+    authUser.user.updateProfile({
+      displayName: username
+    })
+  })
   .catch((error) => alert(error.message));
+}
+
+const signIn = (event) => {
+  event.preventDefault();
+
+  auth.signInWithEmailAndPassword(email, password)
 }
 
   return (
@@ -94,6 +125,36 @@ const SignUp = (event) => {
               </form>
           </div>
       </Modal>
+
+      <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}
+        >
+          <div style={modalStyle} className={classes.paper}>
+            <form className="app_signup">
+            <center>
+              <img
+              className="app_headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+              />
+              </center>
+              <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button onClick={signIn}>Sign In</Button>
+              </form>
+          </div>
+      </Modal>
     
       <div className="app_header">
         <img
@@ -103,7 +164,15 @@ const SignUp = (event) => {
         />
       </div>
 
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+      ):  (
+        <div className="app_loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>Sign Up</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+        </div>
+      )}
+
       
       <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
 
